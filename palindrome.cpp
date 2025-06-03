afe2b73a062104ac9878eca1262c5536aa14a6b4
Silverio Ceron <73544552+SilverCero@users.noreply.github.com>
2025-06-03 09:41:35 -0600

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <string>
 
+bool isPalindrome(std::string str) {
+    for (int i = 0; i < str.length() / 2; i++) {
+        if (str[i] != str[str.length() - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    std::string word;
+    std::cout << "Enter a word: ";
+    std::cin >> word;
+
+    if (isPalindrome(word)) {
+        std::cout << word << " is a palindrome." << std::endl;
+    } else {
+        std::cout << word << " is not a palindrome." << std::endl;
+    }
+
+    return 0;
+}
